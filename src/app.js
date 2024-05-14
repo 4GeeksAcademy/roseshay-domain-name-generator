9c777c6c3907d59774d51c0bdd4e83cfffab7265
roseshay <165499006+roseshay@users.noreply.github.com>
2024-05-14 20:10:57 +0000

    complete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
+import "./index.html";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+function generateDomainNames() {
+  let pronouns = ["the", "our", "my", "how"];
+  let adjectives = ["great", "big", "slimey", "hairy"];
+  let nouns = ["jogger", "raccon", "building", "dog"];
+  let ends = [".com", ".net", ".org", ".us"];
+  const domainNames = [];
+  for (const pronoun of pronouns) {
+    for (const adjective of adjectives) {
+      for (const noun of nouns) {
+        for (const end of ends) {
+          const domainName = `${pronoun}${adjective}${noun}${end}`;
+          domainNames.push(domainName);
+        }
+      }
+    }
+  }
+  return domainNames;
+}
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const button = document.querySelector("#domainbutton");
+  const p = document.querySelector("#domain");
+  button.onclick = () => {
+    p.innerHTML = generateDomainNames().join(
+      '<marquee><i class="fa-solid fa-flag-checkered" style="color: #511F1F;"></i></marquee>'
+    );
+  };
 };
